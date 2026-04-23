620ea6c1eb4618f2e9a74d999d831c874709dee2
jdobie11 <jonathon.dobie@kellogg.northwestern.edu>
2026-04-23 12:13:38 -0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CrapsGame from './CrapsGame.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CrapsGame />
-  </React.StrictMode>
-)
